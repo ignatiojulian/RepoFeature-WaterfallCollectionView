2d976f0de12bfece8fc2e6dbf7ae188f1e8404f2
Ignatio <ignatiojulian@gmail.com>
2021-07-20 22:02:55 +0800

    feature show image

---
+++ waterfall-collectionViewe/waterfall-collectionViewe/ImageCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ImageCollectionViewCell.swift
+//  waterfall-collectionViewe
+//
+//  Created by Ignatio Julian on 20/07/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ImageCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    func configure(image: UIImage?) {
+        imageView.image = image
+    }
+}
--- waterfall-collectionViewe/waterfall-collectionViewe/ViewController.swift
+++ waterfall-collectionViewe/waterfall-collectionViewe/ViewController.swift
@@ -5,15 +5,76 @@
 //  Created by Ignatio Julian on 20/07/21.
 //
 
+import CHTCollectionViewWaterfallLayout
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, CHTCollectionViewDelegateWaterfallLayout {
+    
+    private let collectionView: UICollectionView = {
+        let layout = CHTCollectionViewWaterfallLayout()
+        layout.itemRenderDirection = .leftToRight
+        layout.columnCount = 2
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        
+        return collectionView
+    }()
+    
+    struct Models {
+        let imageName: String
+        let height: CGFloat
+    }
+    
+    private var model = [Models]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let images = Array(1...9).map { "image\($0)" }
+        model = images.compactMap {
+            return Models.init(
+                imageName: $0,
+                height: CGFloat.random(in: 300...600)
+            )
+        }
+        
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: ImageCollectionViewCell.identifier,
+                for: indexPath
+        ) as? ImageCollectionViewCell else {
+            fatalError()
+        }
+        
+        cell.configure(image: UIImage(named: model[indexPath.row].imageName))
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(
+            width: view.frame.size.width/2,
+            height: model[indexPath.row].height)
+    }
 
 }
 
